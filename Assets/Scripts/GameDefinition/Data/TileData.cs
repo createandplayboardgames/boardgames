8b43a66d2ffc0f1daa8338f0f476ca8732b4f8b1
sativaleanne <maciels@oregonstate.edu>
2024-03-13 00:02:25 -0700

    Latest up to date version

--- Assets/Scripts/GameDefinition/Data/TileData.cs
+++ Assets/Scripts/GameDefinition/Data/TileData.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class TileData : MonoBehaviour
 {
