961f9bfb05bea8fb47e10f1f07b0aa41c5fe6c14
Taylor Ballai <taylorballai@gmail.com>
2024-04-17 19:34:03 -0400

    camera now can't move out-of-bounds

--- Assets/Scripts/User Interface/ComponentBehaviors/CameraPanZoom.cs
+++ Assets/Scripts/User Interface/ComponentBehaviors/CameraPanZoom.cs
@@ -7,34 +7,42 @@ public class CameraPanZoom : MonoBehaviour, IScrollHandler, IDragHandler, IPoint
     public Camera mainCamera;
     private float dragScale = .02f;
     private float zoomScale = .5f;
+    private PointerEventData.InputButton panButton = PointerEventData.InputButton.Middle;
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        if (eventData.button != PointerEventData.InputButton.Middle)
+        if (eventData.button != panButton)
             return;
-        if (eventData.button == PointerEventData.InputButton.Middle)
+        if (eventData.button == panButton)
             Cursor.visible = false;
     }
     public void OnPointerUp(PointerEventData eventData)
    {
-        if (eventData.button != PointerEventData.InputButton.Middle)
+        if (eventData.button != panButton)
             return;
-        if (eventData.button == PointerEventData.InputButton.Middle)
+        if (eventData.button == panButton)
             Cursor.visible = true;
     }
     public void OnDrag(PointerEventData eventData)
     {
-        if (eventData.button != PointerEventData.InputButton.Middle)
+        if (eventData.button != panButton)
             return;
+
+        //calculate new position, limit to background boundaries 
+        Vector3 cameraCenter = mainCamera.transform.position;
+        GameObject board = GameObject.Find("Background");
+        Vector3 boardCenter = board.transform.position;
+        float   boardWidth  = GetComponent<SpriteRenderer>().bounds.size.x;
+        float   boardHeight = GetComponent<SpriteRenderer>().bounds.size.y;
         Vector2 amountToChange = eventData.delta * dragScale;
-        mainCamera.transform.Translate(amountToChange.x, -amountToChange.y, 0);
+        float newX = Mathf.Clamp(cameraCenter.x + amountToChange.x, boardCenter.x - boardWidth/2,  boardCenter.x + boardWidth/2);
+        float newY = Mathf.Clamp(cameraCenter.y + amountToChange.y, boardCenter.y - boardHeight/2, boardCenter.y + boardHeight/2);
+        mainCamera.transform.position = new Vector3(newX, newY, cameraCenter.z);
     }
     public void OnScroll(PointerEventData eventData)
     {
         float amountToChange = eventData.scrollDelta.y * zoomScale;
         mainCamera.orthographicSize = Mathf.Clamp(mainCamera.orthographicSize + amountToChange, 1, 10);
     }
-    Vector2 GetMousePos() { return Camera.main.ScreenToWorldPoint(Input.mousePosition); }
-
 
 }
