0db8348a7eef819c5921f969cca3fab9cdb8cbb7
Taylor Ballai <taylorballai@gmail.com>
2024-04-16 20:45:08 -0400

    made camera movement feel a little bit better through hiding the cursor

--- Assets/Scripts/User Interface/ComponentBehaviors/CameraPanZoom.cs
+++ Assets/Scripts/User Interface/ComponentBehaviors/CameraPanZoom.cs
@@ -1,19 +1,33 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CameraPanZoom : MonoBehaviour, IScrollHandler, IDragHandler
+public class CameraPanZoom : MonoBehaviour, IScrollHandler, IDragHandler, IPointerDownHandler, IPointerUpHandler
 {
+
     public Camera mainCamera;
-    private float dragScale = .01f;
+    private float dragScale = .02f;
     private float zoomScale = .5f;
 
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (eventData.button != PointerEventData.InputButton.Middle)
+            return;
+        if (eventData.button == PointerEventData.InputButton.Middle)
+            Cursor.visible = false;
+    }
+    public void OnPointerUp(PointerEventData eventData)
+   {
+        if (eventData.button != PointerEventData.InputButton.Middle)
+            return;
+        if (eventData.button == PointerEventData.InputButton.Middle)
+            Cursor.visible = true;
+    }
     public void OnDrag(PointerEventData eventData)
     {
-        if (eventData.button == PointerEventData.InputButton.Middle)
-        {
-            Vector2 amountToChange = eventData.delta * dragScale;
-            mainCamera.transform.Translate(amountToChange.x, -amountToChange.y, 0);
-        }
+        if (eventData.button != PointerEventData.InputButton.Middle)
+            return;
+        Vector2 amountToChange = eventData.delta * dragScale;
+        mainCamera.transform.Translate(amountToChange.x, -amountToChange.y, 0);
     }
     public void OnScroll(PointerEventData eventData)
     {
