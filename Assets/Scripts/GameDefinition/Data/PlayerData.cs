57f9e0ca23f0c31464e5cb6a9b87ba517967ebb3
sativaleanne <maciels@oregonstate.edu>
2024-05-15 19:48:29 -0700

    Adjustments

--- Assets/Scripts/GameDefinition/Data/PlayerData.cs
+++ Assets/Scripts/GameDefinition/Data/PlayerData.cs
@@ -10,18 +10,18 @@ public class PlayerData : NonTileData {
     public string spritePath;  // Added for sprite save/load
 
     // Method to set data from a SerializablePlayerData object
-    //public void SetData(SerializablePlayerData data)
-    //{
-    //    this.playerName = data.playerName;
-    //    this.spritePath = data.spritePath;
-    //    this.transform.position = data.position;
+    public void SetData(SerializablePlayerData data)
+    {
+        this.playerName = data.playerName;
+        this.spritePath = data.spritePath;
+        this.transform.position = data.position;
 
-    //    // Load the sprite if spritePath is valid
-    //    Sprite newSprite = Resources.Load<Sprite>(data.spritePath);
-    //    if (newSprite)
-    //    {
-    //        this.GetComponent<SpriteRenderer>().sprite = newSprite;
-    //    }
-    //}
+        // Load the sprite if spritePath is valid
+        Sprite newSprite = Resources.Load<Sprite>(data.spritePath);
+        if (newSprite)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = newSprite;
+        }
+    }
 }
 
--- Assets/Scripts/SaveSystem/GameSaveSystem.cs
+++ Assets/Scripts/SaveSystem/GameSaveSystem.cs
@@ -5,102 +5,102 @@ using UnityEngine;
 
 public class GameStateManager : MonoBehaviour
 {
-//    public static GameStateManager Instance;
-//    private GameDefinitionManager gameDefinitionManager;
+    public static GameStateManager Instance;
+    private GameDefinitionManager gameDefinitionManager;
 
-//    private static string savePath = Application.persistentDataPath + "/gameSave.save";
+    private static string savePath = Application.persistentDataPath + "/gameSave.save";
 
-//    private void Awake()
-//    {
-//        if (Instance == null)
-//        {
-//            Instance = this;
-//            DontDestroyOnLoad(gameObject);
-//        }
-//        else
-//        {
-//            Destroy(gameObject);
-//        }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
 
-//        gameDefinitionManager = FindObjectOfType<GameDefinitionManager>();
-//    }
+        gameDefinitionManager = FindObjectOfType<GameDefinitionManager>();
+    }
 
-//    public void SaveGame()
-//    {
-//        if (gameDefinitionManager == null)
-//        {
-//            Debug.LogError("GameDefinitionManager not found!");
-//            return;
-//        }
+    public void SaveGame()
+    {
+        if (gameDefinitionManager == null)
+        {
+            Debug.LogError("GameDefinitionManager not found!");
+            return;
+        }
 
-//        SaveData data = new SaveData
-//        {
-//            players = gameDefinitionManager.cache.players,
-//            tiles = gameDefinitionManager.cache.tiles,
-//            actions = gameDefinitionManager.cache.actions
-//        };
+        //SaveData data = new SaveData
+        //{
+        //    playersData = gameDefinitionManager.cache.players,
+        //    tilesInfo = gameDefinitionManager.cache.tiles,
+        //    //actions = gameDefinitionManager.cache.actions
+        //};
 
-//        BinaryFormatter formatter = new BinaryFormatter();
-//        FileStream stream = new FileStream(savePath, FileMode.Create);
-//        formatter.Serialize(stream, data);
-//        stream.Close();
-//        Debug.Log("Game saved successfully to " + savePath);
-//    }
+        //BinaryFormatter formatter = new BinaryFormatter();
+        //FileStream stream = new FileStream(savePath, FileMode.Create);
+        //formatter.Serialize(stream, data);
+        //stream.Close();
+        //Debug.Log("Game saved successfully to " + savePath);
+    }
 
-//    public void LoadGame()
-//    {
-//        if (!File.Exists(savePath))
-//        {
-//            Debug.LogError("Save file not found in " + savePath);
-//            return;
-//        }
+    public void LoadGame()
+    {
+        if (!File.Exists(savePath))
+        {
+            Debug.LogError("Save file not found in " + savePath);
+            return;
+        }
 
-//        BinaryFormatter formatter = new BinaryFormatter();
-//        FileStream stream = new FileStream(savePath, FileMode.Open);
-//        SaveData data = formatter.Deserialize(stream) as SaveData;
-//        stream.Close();
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream stream = new FileStream(savePath, FileMode.Open);
+        SaveData data = formatter.Deserialize(stream) as SaveData;
+        stream.Close();
 
-//        if (data == null)
-//        {
-//            Debug.LogError("Failed to load game data.");
-//            return;
-//        }
+        if (data == null)
+        {
+            Debug.LogError("Failed to load game data.");
+            return;
+        }
 
-//        // Clear existing game state
-//        gameDefinitionManager.ClearGameState();
+        // Clear existing game state
+    //    gameDefinitionManager.ClearGameState();
 
-//        // Apply the loaded data
-//        ApplyLoadedData(data);
-//        Debug.Log("Game loaded successfully from " + savePath);
-//    }
+    //    // Apply the loaded data
+    //    ApplyLoadedData(data);
+    //    Debug.Log("Game loaded successfully from " + savePath);
+    //}
 
-//    private void ApplyLoadedData(SaveData data)
-//    {
-//        foreach (var playerData in data.players)
-//        {
-//            var player = gameDefinitionManager.CreatePlayer(); // Does CreatePlayer set the player's state?
-//            player.SetData(playerData);
-//        }
+    //private void ApplyLoadedData(SaveData data)
+    //{
+    //    foreach (var playerData in data.players)
+    //    {
+    //        var player = gameDefinitionManager.CreatePlayer(); // Does CreatePlayer set the player's state?
+    //        player.SetData(playerData);
+    //    }
 
-//        foreach (var tileData in data.tiles)
-//        {
-//            var tile = gameDefinitionManager.CreateTile();
-//            tile.SetData(tileData);
-//        }
+    //    foreach (var tileData in data.tiles)
+    //    {
+    //        var tile = gameDefinitionManager.CreateTile();
+    //        tile.SetData(tileData);
+    //    }
 
-//        foreach (var actionData in data.actions)
-//        {
-//            // Depending on the type of action, call the respective method to recreate the action
-//            CreateActionFromData(actionData);
-//        }
-//    }
+    //    foreach (var actionData in data.actions)
+    //    {
+    //        // Depending on the type of action, call the respective method to recreate the action
+    //        CreateActionFromData(actionData);
+    //    }
+    }
 
-//    private void CreateActionFromData(ActionData actionData)
-//    {
-//        // Need to expand this for other actions
-//        if (actionData is FinishGameActionData finishActionData)
-//        {
-//            gameDefinitionManager.CreateFinishGameAction();
-//        }
-//    }
+    private void CreateActionFromData(ActionData actionData)
+    {
+        // Need to expand this for other actions
+        if (actionData is FinishGameActionData finishActionData)
+        {
+            gameDefinitionManager.CreateFinishGameAction();
+        }
+    }
 }
